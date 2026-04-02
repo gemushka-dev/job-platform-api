9f074aebe1e497c14c46e6e12a9b1bc2ca770efe
unknown <iskandar.django.work@gmail.com>
2026-04-02 15:48:27 +0500

    Fix: update auth middleware with adding HttpError

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const HttpError = require("../error/httpError");
 
 module.exports.auth = async (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return res.status(401).send("Not Authenticated");
-  }
   try {
-    const payload = jwt.verify(token, process.env.SECRET);
-    req.user = payload;
-    next();
+    const token = req.cookies.access_token;
+    if (!token) {
+      throw new HttpError("Not Authenticated", 401);
+    }
+    try {
+      const payload = jwt.verify(token, process.env.SECRET);
+      req.user = payload;
+      next();
+    } catch (e) {
+      throw new HttpError("Invalid Token", 401);
+    }
   } catch (e) {
-    res.status(401).send("Invalid Token");
+    next(e);
   }
 };
