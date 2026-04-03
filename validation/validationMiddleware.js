ec09e0cb3004c10e17325b3fa1fb05d87ee63582
unknown <iskandar.django.work@gmail.com>
2026-04-03 18:08:19 +0500

    Validation: add companies schema and middlewares

--- validation/validationMiddleware.js
+++ validation/validationMiddleware.js
@@ -66,3 +66,61 @@ module.exports.workersUpdateValidation = (req, res, next) => {
     next(e);
   }
 };
+
+const companiesRegisterSchema = z.object({
+  companyName: z
+    .string({ message: "Incorrect companyName type" })
+    .min(3, { message: "companyName must be at least 3 characters long" })
+    .max(64),
+  email: z.email({ message: "Incorrect email type" }),
+  password: z
+    .string({ message: "Incorrect password type" })
+    .min(8, { message: "password must be at least 8 characters long" }),
+  description: z.string({ message: "Incorrect description type" }),
+});
+
+const companiesLoginSchema = z.object({
+  email: z.email({ message: "Incorrect email type" }),
+  password: z
+    .string({ message: "Incorrect password type" })
+    .min(8, { message: "password must be at least 8 characters long" }),
+});
+
+const companiesUpdateSchema = z.object({
+  companyName: z
+    .string({ message: "Incorrect companyName type" })
+    .min(3, { message: "companyName must be at least 3 characters long" })
+    .max(64)
+    .optional(),
+  description: z.string({ message: "Incorrect description type" }).optional(),
+});
+
+module.exports.companiesRegisterValidation = (req, res, next) => {
+  try {
+    const validatedData = companiesRegisterSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.companiesLoginValidation = (req, res, next) => {
+  try {
+    const validatedData = companiesLoginSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.companiesUpdateValidation = (req, res, next) => {
+  try {
+    const validatedData = companiesUpdateSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
